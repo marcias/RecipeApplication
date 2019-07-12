e599a5c6358ec0118aaaac83f4f29df83fa6dfaf
Márcia Coutinho <marcia.silveiro@gmail.com>
2019-07-12 17:19:01 +0200

    Populating list of selected images

--- app/src/main/java/com/marciasc/recipeapplication/AddRecipeActivity.java
+++ app/src/main/java/com/marciasc/recipeapplication/AddRecipeActivity.java
@@ -1,18 +1,47 @@
 package com.marciasc.recipeapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class AddRecipeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddRecipeActivity extends AppCompatActivity implements ImageListAdapter.OnRemoveButtonPressed {
+    private final int REQUEST_CODE = 1001;
+    private List<Uri> mListImages = new ArrayList<>();
+    private LinearLayoutManager mLayoutManager;
+    private RecyclerView mRecyclerView;
+    private ImageListAdapter mImageListAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        mImageListAdapter = new ImageListAdapter(this, this);
+        mRecyclerView = findViewById(R.id.rv_selected_images);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mImageListAdapter);
+
+        ImageView ibAddImage = findViewById(R.id.ib_add_image);
+        ibAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage();
+            }
+        });
     }
 
     @Override
@@ -31,4 +60,29 @@ public class AddRecipeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void pickImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    private void removeImage(Uri uri) {
+        mListImages.remove(uri);
+        mImageListAdapter.setList(mListImages);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && data.getData() != null) {
+            mListImages.add(data.getData());
+            mImageListAdapter.setList(mListImages);
+        }
+    }
+
+    @Override
+    public void onRemoveButtonPressed(int position) {
+        removeImage(mListImages.get(position));
+    }
 }
---
+++ app/src/main/java/com/marciasc/recipeapplication/ImageListAdapter.java
@@ -0,0 +1,82 @@
+package com.marciasc.recipeapplication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ImageViewHolder> {
+    private LayoutInflater mLayoutInflater;
+    private List<Uri> mImageList;
+    private Context mContext;
+    private OnRemoveButtonPressed mListener;
+
+    ImageListAdapter(Context context, OnRemoveButtonPressed listener) {
+        mContext = context;
+        mListener = listener;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.image_item_list, parent, false);
+        return new ImageViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, final int position) {
+        if (mImageList != null) {
+            Uri imageUri = mImageList.get(position);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            holder.imageView.setImageBitmap(bitmap);
+        }
+
+        holder.removePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onRemoveButtonPressed(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageList != null ? mImageList.size() : 0;
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private ImageView removePhoto;
+
+        private ImageViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.iv_image);
+            removePhoto = itemView.findViewById(R.id.iv_remove_image);
+        }
+    }
+
+    public void setList(List<Uri> imageList) {
+        mImageList = imageList;
+        notifyDataSetChanged();
+    }
+
+    interface OnRemoveButtonPressed {
+        void onRemoveButtonPressed(int position);
+    }
+}
